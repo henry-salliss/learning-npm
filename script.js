4831737a33503a054b5e221d2ef72c99203c6a1a
Henry Salliss <henrysalliss@gmail.com>
2021-06-28 12:33:06 +0100

    began working with babel and polyfilling some ES6 code

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // import  from './node_modules/lodash-es/_cloneBuffer.js';
 
-// import cloneDeep from './node_modules/lodash-es/cloneDeep.js';
-import cloneDeep from 'lodash-es';
+import cloneDeep from './node_modules/lodash-es/cloneDeep.js';
+// import cloneDeep from 'lodash';
 
 const nested = {
   test: [
@@ -18,6 +18,36 @@ import { totalPrice, totalQuantity, addToCart } from './shoppingCart.js';
 
 addToCart('Bread', 2);
 
+const state = {
+  cart: [
+    { product: 'cereal', quantity: 2 },
+    { product: 'cookies', quantity: 8 },
+  ],
+  user: { loggedIn: true },
+};
+
+const stateClone = Object.assign({}, state);
+const stateDeepClone = cloneDeep(state);
+console.log(stateDeepClone);
+console.log('hello');
+
+class Person {
+  #greeting = 'Hey';
+  constructor(name) {
+    this.name = name;
+    console.log(`${this.#greeting}, ${this.name}`);
+  }
+}
+
+const henry = new Person('Henry');
+Promise.resolve('Test').then(x => console.log(x));
+
+// Just importing polyfilled find method
+import 'core-js/stable/array/find';
+
 if (module.hot) {
   module.hot.accept();
 }
+
+// Polyfilling async functions
+import 'regenerator-runtime/runtime';
